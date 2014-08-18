b48826b9649fc0f0e9e2cd4b4281f1e8d314eb43
nschwertner <nikolai.schwertner@childrens.harvard.edu>
2014-08-18 10:26:35 -0400

    Add missing POST type to bbclient

--- client/bb-client.js
+++ client/bb-client.js
@@ -76,6 +76,7 @@ function completeCodeFlow(params){
   $.ajax({
 
     url: state.provider.oauth2.token_uri,
+    type: 'POST',
     data: {
       code: params.code,
       grant_type: 'authorization_code',
