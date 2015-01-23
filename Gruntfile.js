914cc4513d634c81e7400e1c5a16f95fcf4decab
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2015-01-22 19:27:09 -0500

    Use 6to5ify to transcode ES6 modeules to ES5
    Conflicts:
    
    	dist/fhir-client.min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,14 +15,7 @@ module.exports = function (grunt) {
     },
     shell: {
       browserify: {
-        command: "./node_modules/.bin/browserify  -e client/entry.js  -i 'jsdom' > dist/fhir-client.js",
-        options: {
-          failOnError: true,
-          stderr: true
-        }
-      },
-      '6to5': {
-        command: "sed -i '' 's/const /var /g' dist/fhir-client.js",
+        command: "./node_modules/.bin/browserify  -e client/entry.js  -i 'jsdom' -g 6to5ify > dist/fhir-client.js",
         options: {
           failOnError: true,
           stderr: true
@@ -39,8 +32,7 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('browserify', 'Browserify to create window.FHIR', ['shell:browserify']);
-  grunt.registerTask('6to5', 'Transcode ES6 to ES5', ['shell:6to5']);
   grunt.registerTask('conformance', 'Download conformance base', ['curl:conformance']);
-  grunt.registerTask('default', ['browserify', '6to5', 'uglify:minifiedLib']);
+  grunt.registerTask('default', ['browserify', 'uglify:minifiedLib']);
   grunt.registerTask('all', ['conformance', 'default']);
 };
