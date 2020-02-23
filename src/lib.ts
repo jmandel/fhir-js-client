1490145c0876c4f293997dadbb0ca380968ba61e
Vladimir Ignatov <vlad.ignatov@gmail.com>
2020-02-23 18:25:44 -0500

    setPath can create missing intermediate objects or arrays

--- src/lib.ts
+++ src/lib.ts
@@ -203,17 +203,22 @@ export function getPath(obj: fhirclient.JsonObject, path = ""): any {
 
 /**
  * Like getPath, but if the node is found, its value is set to @value
- * @param obj   The object (or Array) to walk through
- * @param path  The path (eg. "a.b.4.c")
+ * @param obj The object (or Array) to walk through
+ * @param path The path (eg. "a.b.4.c")
  * @param value The value to set
+ * @param createEmpty If true, create missing intermediate objects or arrays
  * @returns The modified object
  */
-export function setPath(obj: fhirclient.JsonObject, path: string, value: any): fhirclient.JsonObject {
+export function setPath(obj: fhirclient.JsonObject, path: string, value: any, createEmpty = false): fhirclient.JsonObject {
     path.trim().split(".").reduce(
         (out, key, idx, arr) => {
             if (out && idx === arr.length - 1) {
                 out[key] = value;
-            } else {
+            }
+            else {
+                if (out && out[key] === undefined && createEmpty) {
+                    out[key] = arr[idx + 1].match(/^[0-9]+$/) ? [] : {};
+                }
                 return out ? out[key] : undefined;
             }
         },
@@ -431,7 +436,7 @@ export async function getTargetWindow(target: fhirclient.WindowTarget, width: nu
 
     // New tab or window
     if (target == "_blank") {
-        let error, targetWindow: Window | null = null;;
+        let error, targetWindow: Window | null = null;
         try {
             targetWindow = window.open("", "SMARTAuthPopup");
             if (!targetWindow) {
